916f820b1ac7fa74fa877b235e74ec0195564e0b
Glitch (shade-continuous-dress) <none>
2020-05-03 15:32:37 +0000

    test

---
+++ controllers/login.controller.js
@@ -0,0 +1,40 @@
+var db = require('../db')
+
+module.exports.index = function(req,res,next){
+  res.render('login/index')
+};
+
+module.exports.postLogin = function(req,res,next){
+  var email = req.body.email;
+  var password = req.body.password;
+  
+  var user = db.get('users').find({email:email}).value();
+  if (!user){
+    res.render('login/index',{
+      errors: [
+        'User does not exist'
+      ],
+      values: req.body
+    })
+    return;
+  }
+  if (user.password !== password){
+    res.render('login/index',{
+      errors: [
+        'Wrong password'
+      ],
+      values: req.body
+    })
+    return;
+  }
+  next();
+};
+module.exports.loginTransaction = function(req,res){
+  var email = req.body.email;
+  var name = db.get('users').find({email:email}).value();
+  var tran = db.get('transactions').filter({userId:name.name}).value();
+  res.cookie('userID',name.userId);
+  res.render('transactions/index',{
+    trans: tran
+  })
+};
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var userRouter = require('./routers/user.router');
 var bookRouter = require('./routers/book.router');
-var transactionRouter = require('./routers/transaction.router')
+var transactionRouter = require('./routers/transaction.router');
+var loginRouter = require('./routers/login.router');
+
+var middleware = require('./middleware/auth.middleware');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.set('view engine','pug');
 app.set('views','./views');
 
-app.use(express.static("public"));
-
 app.get('/',(req,res)=>{
+  res.cookie("Duy",1234);
   res.render('index.pug');
 });
 
-app.use('/users', userRouter);
+var count=0;
+function cookieCount(req,res,next){
+  count++;
+  console.log(req.cookies,":",count);
+  next();
+}
 
-app.use('/books', bookRouter);
+app.use('/users',middleware.auth, cookieCount, userRouter);
 
-app.use('/transactions', transactionRouter);
+app.use('/books', cookieCount, bookRouter);
+
+app.use('/transactions',middleware.auth, cookieCount, transactionRouter);
+
+app.use('/login', loginRouter);
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
