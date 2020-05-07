403f52c2f601f47cb3cf998c52a0321714741548
Glitch (succinct-level-hacksaw) <none>
2020-05-07 03:18:34 +0000

    hashed password by bcrypt

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,15 +1,26 @@
+const bcrypt = require('bcrypt');
 var db = require('../db')
 
 module.exports.index = function(req,res,next){
-  res.render('login/index')
+    res.render('login/index')
 };
 
 module.exports.postLogin = function(req,res,next){
   var email = req.body.email;
-  var password = req.body.password;
-  
+  var password = req.body.password;  
   var user = db.get('users').find({email:email}).value();
-  console.log(user);
+  var numberLogin = user.wrongLoginCount;
+  var numberCount = db.get('users').value().map((user)=>{return user.wrongLoginCount});
+  // for (var count in numberCount){
+  //   if( count >= 4){
+  //     res.render('login/index',{
+  //       errors:[
+  //         'Hack?'
+  //       ]
+  //     })
+  //     return
+  //   }
+  // }
   if (!user){
     res.render('login/index',{
       errors: [
@@ -19,22 +30,21 @@ module.exports.postLogin = function(req,res,next){
     })
     return;
   }
-  if (user.password !== password){
-    res.render('login/index',{
-      errors: [
-        'Wrong password'
-      ],
-      values: req.body
-    })
-    return;
-  }
-  res.cookie('userId',user.id);
-  res.redirect('/');
-};
-// module.exports.loginTransaction = function(req,res){
-//   var email = req.body.email;
-//   var name = db.get('users').find({email:email}).value();
-//   var tran = db.get('transactions').filter({userId:name.name}).value();
-//   res.cookie('userID',name.userId);
-//   res.redirect('/');
-// };
+  bcrypt.compare(password, user.password, function(err, result) {
+    if(result == true) {
+      db.get('users').find({email:email}).assign({wrongLoginCount:0}).value();
+      res.cookie('userId',user.id);
+      res.redirect('/');
+    } else {
+      numberLogin += 1;
+      db.get('users').find({email:email}).assign({wrongLoginCount:numberLogin}).value();
+      res.render('login/index',{
+        errors: [
+          'Wrong password'
+        ],
+        values: req.body
+      })
+      return;
+    }
+  });
+}
