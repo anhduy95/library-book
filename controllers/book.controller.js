837ff688003d5fa7b75b3abd0dbef017eebe37f3
Glitch (wool-faint-molybdenum) <none>
2020-05-10 18:47:34 +0000

    pagination books

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -2,8 +2,16 @@ const shortid = require('shortid');
 var db = require('../db.js');
 
 module.exports.index = function(req,res){
-  res.render('books/index.pug',{
-    books: db.get('books').value()
+  var page = parseInt(req.query.page) || 1 ;
+  var previousPage = page - 1;
+  var nextPage = page + 1;
+  var start = (page - 1) * 8;
+  var end = page * 8;
+  res.render('books/index',{
+    books : db.get('books').value().slice(start,end),
+    page: page,
+    previousPage: previousPage,
+    nextPage: nextPage,
   })
 };
 module.exports.create = function(req,res){
@@ -16,8 +24,8 @@ module.exports.postCreate = function(req,res){
   
   res.redirect('/books')
 };
-module.exports.delete = function (req,res){
-  var id = req.params.id;
-  db.get('books').remove({id:id}).write();
-  res.redirect('/books');
-};
+// module.exports.delete = function (req,res){
+//   var id = req.params.id;
+//   db.get('books').remove({id:id}).write();
+//   res.redirect('/books');
+// };
